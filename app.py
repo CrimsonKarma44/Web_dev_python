7a862ea4dc010c2bdef54365da842ca704683996
Okereke Princewill <vincentprincewill44@gmail.com>
2023-03-12 23:20:17 +0100

    New

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def Hello_world():
-    return "Hello, wrld!"
+    return render_template("home_page.html")
+
+@app.route("/jobs_done/")
+
+def works_page():
+    return render_template('Experiences.html')
 
 if __name__ == "__main__":
-    app.run(port)
+    app.run(debug=True)
